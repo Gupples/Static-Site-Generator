0c7faebaa705463a529264e55dc0e7518e787861
Gupples <gupples224@gmail.com>
2026-02-12 19:23:20 -0800

    Created detection function for block type and tests.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import re
+
 def markdown_to_blocks(markdown):
     raw_blocks = markdown.split("\n\n")
     new_blocks = []
@@ -7,3 +10,24 @@ def markdown_to_blocks(markdown):
         if block != "":
             new_blocks.append(block)
     return new_blocks
+
+BlockType = Enum("BlockType", ["PARAGRAPH", "HEADING", "CODE", "QUOTE", "UNORDERED_LIST", "ORDERED_LIST"])
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split('\n')
+    if all(line.startswith('>') for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith('- ') for line in lines):
+        return BlockType.UNORDERED_LIST
+    is_ordered_list = True
+    for i, line in enumerate(lines, start=1):
+        if not line.startswith(f"{i}. "):
+            is_ordered_list = False
+            break
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -79,3 +79,206 @@ This is the same paragraph on a new line
         blocks = markdown_to_blocks(md)
         self.assertEqual(len(blocks), 3)
         self.assertListEqual(blocks, expected)
+
+
+    def test_block_to_block_type_paragraph_one_line(self):
+        md = """
+This is a normal paragraph.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_multiple_lines_solid(self):
+        md = """
+This is a normal paragraph.
+It has a number of lines.
+They are all together.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_code_without_start(self):
+        md = """
+This is supposed to be a code block,
+but it's missing the first three '`' characters.
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_code_without_end(self):
+        md = """
+```
+This is supposed to be a code block,
+but it's missing the last three '`' characters.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph_inconsistent_quotes(self):
+        md = """
+> This would normally be a quote block,
+> but it is broken by
+this single line.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_inconsistent_unordered_list(self):
+        md = """
+- This would normally be an unordered list
+- but it is broken by
+this single line.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_unordered_list_no_space(self):
+        md = """
+- This would normally be an unordered list
+- but it is broken by
+-this single line.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_inconsistent_ordered_list(self):
+        md = """
+1. This would normally be an unordered list
+2. but it is broken by
+this single line.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_constant_ordered_list(self):
+        md = """
+1. This would normally be an unordered list
+1. but it is broken by
+1. all the ones in the list.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_ordered_list_starts_by_0(self):
+        md = """
+0. This would normally be an unordered list
+1. but it is broken by
+2. starting with 0.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_too_many_hashes(self):
+        md = """
+####### There are 7 hashes in this line. The parameter is 6 and a space.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_paragraph_heading_no_space(self):
+        md = """
+######There are 6 hashes in this line. The parameter is 6 and a space.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+        
+    def test_block_to_block_type_heading_one_line(self):
+        md = """
+###### This should be a single-lined header.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.HEADING)
+        
+    def test_block_to_block_type_heading_many_lines(self):
+        md = """
+###### This should be a multi-lined header.
+It should not matter that this line does not start with hashes.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.HEADING)
+        
+    def test_block_to_block_type_code_no_content(self):
+        md = """
+``````
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.CODE)
+        
+    def test_block_to_block_type_code_one_line(self):
+        md = """
+```
+This should still be code.
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.CODE)
+        
+    def test_block_to_block_type_code_multiple_lines(self):
+        md = """
+```
+This should still be code.
+It should not matter; this should still be code.
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.CODE)
+        
+    def test_block_to_block_type_quote_no_content(self):
+        md = """
+>
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.QUOTE)
+        
+    def test_block_to_block_type_quote_one_line(self):
+        md = """
+> This should be a quote
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.QUOTE)
+        
+    def test_block_to_block_type_quote_multiple_lines(self):
+        md = """
+> This should be a quote.
+> This should still be a quote.
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.QUOTE)
+        
+    def test_block_to_block_type_unordered_list_no_content(self):
+        md = "- "
+        block_type = block_to_block_type(md)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+        
+    def test_block_to_block_type_unordered_list_one_item(self):
+        md = """
+- Unordered item #1
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+        
+    def test_block_to_block_type_unordered_list_multiple_items(self):
+        md = """
+- Unordered item #1
+- Unordered item #2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+        
+    def test_block_to_block_type_ordered_list_one_item(self):
+        md = """
+1. First item
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+        
+    def test_block_to_block_type_ordered_list_multiple_items(self):
+        md = """
+1. First item
+2. Second item
+3. Third item
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+    
