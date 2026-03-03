0d78e9d434135335021078ede5b5a18262e23484
Gupples <gupples224@gmail.com>
2026-03-02 17:01:33 -0800

    Created main functions to copy static files to public directory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
+def replace_files(source, destination):
+    # Get the contents of the directory
+    contents = os.listdir(source)
+
+    # Create the source directory if it doesn't exist
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+
+    # Copy items
+    for item in contents:
+        from_filepath = os.path.join(source, item)
+        print(f"Copying item '{from_filepath}'.")
+        to_filepath = os.path.join(destination, item)
+        if os.path.isfile(from_filepath):
+            shutil.copy(from_filepath, to_filepath)
+        else:
+            replace_files(from_filepath, to_filepath)
+
 def main():
-    dummy_node = TextNode("This is the initial text", TextType("bold"), "https://url.com")
-    dummy_node.__repr__()
+    # Refresh public contents
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    replace_files("static", "public")
 
 
 if __name__ == "__main__":
